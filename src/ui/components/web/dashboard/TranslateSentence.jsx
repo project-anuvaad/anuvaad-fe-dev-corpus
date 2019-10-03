b741110d299f6e882a87d3ea0d3f7da46a3464e6
sajish <sajish.sasidharan@tarento.com>
2019-10-03 16:22:10 +0530

    added multimodel select option in dev user

---
+++ src/ui/components/web/dashboard/TranslateSentence.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+import { white, blueGrey50,darkBlack } from "material-ui/styles/colors";
+import { LineChart, Line, ResponsiveContainer } from "recharts";
+import { typography } from "material-ui/styles";
+import Typography from '@material-ui/core/Typography';
+const TranslateSentence = props => {
+  const { title, data, role } = props;
+  const styles = {
+    paper: {
+      backgroundColor: blueGrey50,
+      // height: 150
+    },
+    div: {
+      // height: 95,
+      padding: "5px 15px 15px 15px",
+      color: darkBlack,
+
+    },
+    header: {
+      fontSize: 24,
+      fontWeight: typography.fontWeightLight,
+      color: darkBlack,
+      backgroundColor: blueGrey50,
+      padding: 10,
+      marginLeft:"55px"
+    }
+  };
+
+  return (
+    <Paper style={styles.paper}>
+      <div style={{ ...styles.header}}>{title}</div>
+      <div style={styles.div}>
+        <ResponsiveContainer>
+          <div>
+          <Typography variant="subtitle1" gutterBottom style={{ color: darkBlack,marginLeft:"75px"}} >{data ? data: ''}</Typography>
+          </div>
+        </ResponsiveContainer>
+      </div>
+    </Paper>
+  );
+};
+
+// NewOrders.propTypes = {
+//   data: PropTypes.array
+// };
+
+export default TranslateSentence;
