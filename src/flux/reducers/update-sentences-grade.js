a4202a7a9fbd910d4553b0de7b0943b3a14d8bd0
sajish <sajish.sasidharan@tarento.com>
2019-10-15 12:25:50 +0530

    fixed total score issue

--- src/flux/reducers/update-sentences-grade.js
+++ src/flux/reducers/update-sentences-grade.js
@@ -1,6 +1,7 @@
 import C from '../actions/constants';
 
 export default function (state = {}, action) {
+    console.log(action.type)
     switch (action.type) {
         case C.UPDATE_SENTENCE_GRADE:
             return action.payload;
