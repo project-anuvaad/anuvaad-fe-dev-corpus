56e2d00eb4127b95b03738ec3de2bfe0087c9cf2
aroop <aroop.ghosh@tarento.com>
2019-10-31 16:53:21 +0530

    label changed

--- src/ui/containers/web/GraderViewBenchmark.jsx
+++ src/ui/containers/web/GraderViewBenchmark.jsx
@@ -12,8 +12,8 @@ import Typography from "@material-ui/core/Typography";
 import MenuItem from "@material-ui/core/MenuItem";
 import Button from "@material-ui/core/Button";
 import UpdateSentencesGrade from "../../../flux/actions/apis/upgrade-sentence-grade";
- import AppBar from "../../components/web/common/Appbar";
- import Divider from "@material-ui/core/Divider";
+import AppBar from "../../components/web/common/Appbar";
+import Divider from "@material-ui/core/Divider";
 
 class BenchmarkGrade extends React.Component {
   constructor(props) {
@@ -80,7 +80,7 @@ class BenchmarkGrade extends React.Component {
 
   handleSubmit = () => {
     let api = new UpdateSentencesGrade(this.state.sentences, this.props.match.params.modelid);
-    this.setState({ dialogOpen: false, apiCall: true,showLoader: true, tocken: false ,sentences: []});
+    this.setState({ dialogOpen: false, apiCall: true, showLoader: true, tocken: false, sentences: [] });
     this.props.APITransport(api);
   };
 
@@ -118,17 +118,17 @@ class BenchmarkGrade extends React.Component {
     }
 
     if (prevProps.updateGrade !== this.props.updateGrade) {
-        let api1 = new FetchBenchmarkCompareModel(
-            this.state.base,
-    
-            this.state.pageCount,
-            this.state.offset + 2,
-            this.state.inputStatus
-          );
-        this.setState({ showLoader: true,offset:this.state.offset+1,sentences: []});
-        this.props.APITransport(api1);
-        
-      }
+      let api1 = new FetchBenchmarkCompareModel(
+        this.state.base,
+
+        this.state.pageCount,
+        this.state.offset + 2,
+        this.state.inputStatus
+      );
+      this.setState({ showLoader: true, offset: this.state.offset + 1, sentences: [] });
+      this.props.APITransport(api1);
+
+    }
 
     if (prevProps.fetchBenchmarkCompareModel !== this.props.fetchBenchmarkCompareModel) {
 
@@ -150,7 +150,7 @@ class BenchmarkGrade extends React.Component {
     let sentences = this.state.sentences
     sentences[index][name] = nextValue
     this.setState({ sentences: sentences });
-   
+
   }
 
 
@@ -158,14 +158,14 @@ class BenchmarkGrade extends React.Component {
   render() {
     return (
       <div>
-          <Typography variant="title" color="inherit"style={{ marginTop: "20px",marginLeft:'20px' }} ><b>Benchmark Sentences</b></Typography>
+        <Typography variant="title" color="inherit" style={{ marginTop: "20px", marginLeft: '20px' }} ><b>{"Sentences From " + this.props.label}</b></Typography>
         <Grid container spacing={4} style={{ padding: "20px" }}>
-        {this.state.sentences && this.state.sentences.length > 0 &&<AppBar pending={this.state.pending} count={this.state.count} />}
-        {this.state.sentences.length > 0&& <Typography variant="title" color="inherit"style={{ marginTop: "20px" }} ><b>Source Sentence</b><br/></Typography>}
-        <Grid item xs={12} sm={12} lg={12} xl={12}>
-        <Typography variant="body1" color="inherit" style={{ marginTop: "20px" }} > {this.state.sentences.length > 0 && this.state.sentences[0].source}</Typography>
-        </Grid>
-        
+          {this.state.sentences && this.state.sentences.length > 0 && <AppBar pending={this.state.pending} count={this.state.count} />}
+          {this.state.sentences.length > 0 && <Typography variant="title" color="inherit" style={{ marginTop: "20px" }} ><b>Source Sentence</b><br /></Typography>}
+          <Grid item xs={12} sm={12} lg={12} xl={12}>
+            <Typography variant="body1" color="inherit" style={{ marginTop: "20px" }} > {this.state.sentences.length > 0 && this.state.sentences[0].source}</Typography>
+          </Grid>
+
           {this.state.sentences.map((value, i) => {
             var val = i == 0 ? "A" : "B";
             return <Grid item xs={6} sm={6} lg={6} xl={6}>
@@ -174,34 +174,34 @@ class BenchmarkGrade extends React.Component {
           })}
         </Grid>
         {/*  */}
-        
-        {this.state.sentences && this.state.sentences.length > 0 &&  <Toolbar style={{ marginRight: "3%", marginTop: "20px" }}>
-              <Typography variant="title" color="inherit" style={{ flex: 1 }}></Typography>
-              <Button
-                variant="contained"
-                disabled ={this.state.sentences.length>0 ? this.state.sentences[0].rating && this.state.sentences[1].rating && this.state.sentences[0].context_rating && this.state.sentences[1].context_rating && this.state.sentences[0].spelling_rating && this.state.sentences[1].spelling_rating? false:true: true}
-                onClick={event => {
-                  this.handleSubmit(this.state.sentences);
-                }}
-                color={"primary"}
-                aria-label="edit"
-                style={{ width: "170px", marginBottom: "4%", marginTop: "1px" }}
-              >
-                Save & Next
+
+        {this.state.sentences && this.state.sentences.length > 0 && <Toolbar style={{ marginRight: "3%", marginTop: "20px" }}>
+          <Typography variant="title" color="inherit" style={{ flex: 1 }}></Typography>
+          <Button
+            variant="contained"
+            disabled={this.state.sentences.length > 0 ? this.state.sentences[0].rating && this.state.sentences[1].rating && this.state.sentences[0].context_rating && this.state.sentences[1].context_rating && this.state.sentences[0].spelling_rating && this.state.sentences[1].spelling_rating ? false : true : true}
+            onClick={event => {
+              this.handleSubmit(this.state.sentences);
+            }}
+            color={"primary"}
+            aria-label="edit"
+            style={{ width: "170px", marginBottom: "4%", marginTop: "1px" }}
+          >
+            Save & Next
               </Button>
-            </Toolbar>}
-        {this.state.sentences && this.state.sentences.length > 0 && 
-        <Pagination
-          align="right"
-          limit={1}
-          offset={this.state.offset}
-          centerRipple={true}
-          total={this.state.count}
-          onClick={(event, offset) => {
-            this.handleChangePage(event, offset);
-          }}
-        />
-      }
+        </Toolbar>}
+        {this.state.sentences && this.state.sentences.length > 0 &&
+          <Pagination
+            align="right"
+            limit={1}
+            offset={this.state.offset}
+            centerRipple={true}
+            total={this.state.count}
+            onClick={(event, offset) => {
+              this.handleChangePage(event, offset);
+            }}
+          />
+        }
 
       </div>
     );
