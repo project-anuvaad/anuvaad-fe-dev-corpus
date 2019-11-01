2401531fa94166440343a9b09ff823a98e4ef472
aroop <aroop@netopss-MacBook-Pro-5.local>
2019-11-01 17:03:03 +0530

    .

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -1,12 +1,12 @@
 const configs = {
-    BASE_URL: 'http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com/app/',
+    BASE_URL: 'http://ct.sci-suvas.org:8080/app/',
     AUTH_ENDPOINT: 'oauth2/authorize',
     LOGOUT_ENDPOINT: 'logout',
     POST_LOGOUT_URL:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/',
     RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     RESPONSE_TYPE: 'response_type=token',
     //Test
-    CLIENT_ID: 'client_id=ef79a009-444e-4de4-90c4-52a17ec783f9',
+    CLIENT_ID: 'client_id=e531b5b8-120d-46ba-8167-1f9d7cab2d58',
     //Prod
     // CLIENT_ID: 'client_id=55952c35-8387-4c56-8cf4-a3c1c63714eb',
     REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
