dda94a2e859b20baf9d31feade39bc37ae6883d7
aroop <aroop.ghosh@tarento.com>
2019-11-04 12:09:46 +0530

    config changed

--- src/configs/apigw.js
+++ src/configs/apigw.js
@@ -1,14 +1,14 @@
 const configs = {
-    BASE_URL: 'http://ct.sci-suvas.org:8080/app/',
+    BASE_URL: (process.env.REACT_APP_APIGW_BASE_URL ? process.env.REACT_APP_APIGW_BASE_URL : 'http://nlp-nmt-160078446.us-west-2.elb.amazonaws.com')+ '/app/',
     AUTH_ENDPOINT: 'oauth2/authorize',
     LOGOUT_ENDPOINT: 'logout',
-    POST_LOGOUT_URL:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/',
+    POST_LOGOUT_URL:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : ''),
     RETURN_TO:'returnTo='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     RESPONSE_TYPE: 'response_type=token',
     //Test
-    CLIENT_ID: 'client_id=e531b5b8-120d-46ba-8167-1f9d7cab2d58',
+    CLIENT_ID: 'client_id='+(process.env.REACT_APP_CLIENT_ID ? process.env.REACT_APP_CLIENT_ID:'ef79a009-444e-4de4-90c4-52a17ec783f9'),
     //Prod
-    // CLIENT_ID: 'client_id=55952c35-8387-4c56-8cf4-a3c1c63714eb',
+    // CLIENT_ID: 'client_id=ce88c83f-7e87-403b-95cf-4eefe80da72e',
     REDIRECT_URI: 'redirect_uri='+window.location.protocol + '//' + window.location.hostname + (window.location.port ? (':' + window.location.port) : '')+'/callback',
     
 };
