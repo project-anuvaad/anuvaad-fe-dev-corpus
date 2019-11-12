4381fc66de0e40edabb0adad637da5e5bd9c3396
aroop <aroop.ghosh@tarento.com>
2019-11-12 12:29:12 +0530

    president component added

---
+++ src/ui/components/web/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+class AppCard extends React.Component {
+    render() {
+        let { text } = this.props
+        return (
+            <Card className='card'>
+                <CardContent>
+                    <Typography className='cardTitle' color="textSecondary" gutterBottom>
+                        {text}
+            </Typography>
+                </CardContent>
+            </Card>
+        );
+    }
+}
+
+export default AppCard;
