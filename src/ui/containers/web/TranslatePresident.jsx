a5cf26c3f13e46681289f77148c10a7d6b823a57
aroop <aroop@netopss-MBP-5.Dlink>
2019-11-16 13:58:09 +0530

    card removed

--- src/ui/containers/web/TranslatePresident.jsx
+++ src/ui/containers/web/TranslatePresident.jsx
@@ -4,7 +4,6 @@ import Button from "@material-ui/core/Button";
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import Card from '../../components/web/common/ZoomCard';
 import $ from 'jquery';
 
 import AppCard from '../../components/web/Card';
