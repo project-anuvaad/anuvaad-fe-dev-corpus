0453bc2b508ecb1299030418ad2ef08974d885db
aroop <aroop.ghosh@tarento.com>
2019-11-21 18:01:07 +0530

    logo added

--- src/ui/containers/web/Layout.jsx
+++ src/ui/containers/web/Layout.jsx
@@ -10,14 +10,17 @@ import Snackbars from '../../components/web/common/Snackbar'
 import Theme from "../../theme/web/theme-default";
 import APITransport from "../../../flux/actions/apitransport/apitransport";
 import history from "../../../web.history";
+import logo from '../../../assets/logo.png'
 import UserAuth from "../../../flux/actions/apis/userprofile";
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       userName: "",
-      tocken: false
+      tocken: false,
+      matches: window.matchMedia("(max-width: 1120px)").matches
     };
   }
 
@@ -63,6 +66,23 @@ class App extends React.Component {
           }
           <Header classes={classes} theme={theme} title={title} drawer={drawer} tocken={this.state.tocken} handleTockenChange={this.handleTockenChange.bind(this)} />
           <div className={forDemo ? classes.containerDemo : classes.container} onClick={this.handlDrawerTocken.bind(this)}>
+            {forDemo &&
+              <div style={{
+                position: 'absolute',
+                height: '100%',
+                width: '44%',
+                marginLeft: this.state.matches ? '23%' : '25%'
+              }}>
+                <img src={logo} style={{
+                  width: '70%',
+                  height: '80%',
+                  opacity: '0.3',
+                  display: 'block',
+                  marginLeft: 'auto',
+                  marginRight: 'auto'
+                }} />
+              </div>
+            }
             <Component />
           </div>
           {this.renderMessage()}
