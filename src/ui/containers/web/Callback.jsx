c1ee31dd5b5a7058bdb4410909c1ef4da4b48677
mrinali488 <mrinali.sna@gmail.com>
2020-02-11 16:39:23 +0530

    new localization key added

--- src/ui/containers/web/Callback.jsx
+++ src/ui/containers/web/Callback.jsx
@@ -6,8 +6,18 @@ import UserAuth from "../../../flux/actions/apis/userprofile";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { translate } from '../../../assets/localisation';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { withStyles, MuiThemeProvider } from "@material-ui/core/styles";
+import Theme from "../../theme/web/theme-default";
 
 class Callback extends React.Component {
+  constructor(props){
+    super(props)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
+  }
   componentDidUpdate(prevProps) {
     if (prevProps.userProfile !== this.props.userProfile) {
       if (this.props.userProfile.isActive) {
@@ -36,7 +46,23 @@ class Callback extends React.Component {
   }
 
   componentDidMount() {
-    let hash = this.props.location.hash.split("&");
+    // let hash = this.props.location.hash.split("&");
+    // hash.map(h => {
+    //   if (h.indexOf("access_token") > 0) {
+    //     localStorage.setItem("token", h.split("access_token=")[1]);
+    //     let api = new UserAuth();
+    //     this.props.APITransport(api);
+    //     // history.push(`${process.env.PUBLIC_URL}/corpus`)
+    //   } else if (h.indexOf("error") > 0) {
+    //     localStorage.removeItem("token");
+    //     history.push(`${process.env.PUBLIC_URL}/logout`);
+    //   }
+    // });
+  }
+  handleButtonClick(lang)
+  {
+      localStorage.setItem("lang", lang)
+      let hash = this.props.location.hash.split("&");
     hash.map(h => {
       if (h.indexOf("access_token") > 0) {
         localStorage.setItem("token", h.split("access_token=")[1]);
@@ -51,24 +77,66 @@ class Callback extends React.Component {
   }
 
   render() {
-    return <div>{translate('common.page.text.rediecting')}</div>;
+    return (
+      <MuiThemeProvider theme={Theme}>
+      <Paper style={{ marginLeft: "30%", width: "30%", marginTop: "4%", textAlign:'center',padding:'5%' }} >
+        <Grid container spacing={8}  style={{paddingTop: '4%'}}>
+        <Grid item xs={12} sm={12} lg={12} xl={12}  >
+          <Typography  variant="title">
+            Select your Preffered Language{" "}
+          </Typography>
+        </Grid>
+        </Grid>
+        <Grid container spacing={8} style={{marginTop: '4%'}}>
+        <Grid item xs={12} sm={12} lg={12} xl={12}>
+       
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={event => {
+              this.handleButtonClick('en');
+            }}
+          >
+            English
+    </Button>
+   
+        </Grid>
+        </Grid>
+        <Grid container spacing={8} style={{marginTop: '4%'}}>
+        <Grid item xs={12} sm={12} lg={12} xl={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            fullWidth
+            onClick={event => {
+              this.handleButtonClick('hi');
+            }}
+          >
+            हिन्दी
+      </Button>
+        </Grid>
+        </Grid>
+      </Paper>
+      </MuiThemeProvider>
+    );
   }
-}
+  }
 
-const mapStateToProps = state => ({
-  apistatus: state.apistatus,
-  userProfile: state.userProfile
-});
+  const mapStateToProps = state => ({
+    apistatus: state.apistatus,
+    userProfile: state.userProfile
+  });
 
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(
-    {
-      APITransport
-    },
-    dispatch
-  );
+  const mapDispatchToProps = dispatch =>
+    bindActionCreators(
+      {
+        APITransport
+      },
+      dispatch
+    );
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Callback);
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Callback);
