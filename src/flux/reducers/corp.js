9c4c9401af775f79697145ae74fa972f75d49ecb
sajish <sajish.sasidharan@tarento.com>
2020-02-12 14:21:51 +0530

    fixed date sort issue in benchmark and corpus list page

--- src/flux/reducers/corp.js
+++ src/flux/reducers/corp.js
@@ -4,12 +4,16 @@ export default function (state = {}, action) {
     switch (action.type) {
         case C.FETCH_CORP:
             if(action.payload && Array.isArray(action.payload)){
-                    action.payload.map((t)=>{
-                        var myDate = new Date(t.created_on + ' GMT');
-                        t.created_on = (myDate.toLocaleString('en-UK', {month:'2-digit',day:'2-digit',year:'numeric', hour: 'numeric', minute: 'numeric',second:'numeric', hour12: false }))
-            })
-                }
+                action.payload.map((t)=>{
+                    var myDate = new Date(t.created_at);
+                    console.log(myDate)
+                    t.created_at = (myDate.toLocaleString('en-IN', {day:'2-digit',month:'2-digit',year:'numeric', hour: 'numeric', minute: 'numeric',second:'numeric', hour12: false }))
+        })
+            }
             return action.payload;
+
+
+           
 
         default:
             return state;
