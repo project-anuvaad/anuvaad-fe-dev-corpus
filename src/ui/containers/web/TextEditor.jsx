cb0b5708712406b430c6558b5a1372c5ab0589f0
sajish <sajish.sasidharan@tarento.com>
2020-02-14 16:16:04 +0530

    added newly extracted and existing sentence count

--- src/ui/containers/web/TextEditor.jsx
+++ src/ui/containers/web/TextEditor.jsx
@@ -6,39 +6,35 @@ import { withStyles } from "@material-ui/core";
 import MUIRichTextEditor from "mui-rte";
 import Paper from "@material-ui/core/Paper";
 import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import NewCorpusStyle from "../../styles/web/Newcorpus";
-import APITransport from "../../../flux/actions/apitransport/apitransport";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import { convertToRaw } from "draft-js";
+import draftToMarkdown from "draftjs-to-markdown";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import CKEditor from "@ckeditor/ckeditor5-react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
 import FetchModels from "../../../flux/actions/apis/fetchenchmarkmodel";
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import { convertToRaw } from 'draft-js';
-import draftToMarkdown from 'draftjs-to-markdown';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import CKEditor from '@ckeditor/ckeditor5-react';
-
-
-
+import APITransport from "../../../flux/actions/apitransport/apitransport";
+import NewCorpusStyle from "../../styles/web/Newcorpus";
+import TextareaAutocomplete from 'react-textarea-autocomplete'
 class Editor1 extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { editorHtml: "", theme: "snow", value:'testnpm install --save react-draft-wysiwyg draft-js' };
-    this.handleChange = this.handleChange.bind(this);
+    this.state = { token: false, value: "" };
   }
 
-
   componentDidMount() {
-
     const { APITransport } = this.props;
-    const api = new FetchModels(1573290229, 17,1,1);
+    const api = new FetchModels(1573290229, 17, 1, 1);
     APITransport(api);
-    this.setState({ showLoader: true })
+    this.setState({ showLoader: true });
   }
 
   componentDidUpdate(prevProps) {
-    
-
     if (prevProps.fetchBenchmarkModel !== this.props.fetchBenchmarkModel) {
-        console.log("-----",this.props.fetchBenchmarkModel.data)
+      console.log("-----", this.props.fetchBenchmarkModel.data);
       this.setState({
         sentences: this.props.fetchBenchmarkModel.data,
         count: this.props.fetchBenchmarkModel.count,
@@ -46,28 +42,25 @@ class Editor1 extends React.Component {
       });
     }
   }
-  
 
+  handleSelected=(text,index)=>{
+      console.log(text,index)
+      this.setState({value: this.state.value+text , token: false})
+  }
 
-  handleClick=()=>{
+  handleApiCall(data) {
+    const { APITransport } = this.props;
+    const api = new FetchModels(1573290229, 17, 5, 1);
+    APITransport(api);
+    this.setState({ token: true });
     
-      console.log("event,index,source")
-  }
-  onEditorTab=(data)=> {
-    console.log("gg")
   }
 
-  handleChange(data) {
-    console.log(data);
+  onEditorStateChange(editorState) {
+    console.log(editorState && draftToMarkdown(convertToRaw(editorState.getCurrentContent())));
+    this.setState({ value: editorState });
+    console.log(this.state.value);
   }
-
-  onEditorStateChange(editorState){
-      console.log(editorState && draftToMarkdown(convertToRaw(editorState.getCurrentContent())))
-      this.setState({value: editorState})
-    console.log(this.state.value)
-  };
-
- 
 
   handleSave(data) {
     console.log("data.Model.ImmutableData.EditorState");
@@ -75,49 +68,55 @@ class Editor1 extends React.Component {
 
   render() {
     return (
-      
-          <CKEditor editor={ ClassicEditor } data={this.state.value} onChange={ ( event, editor ) => {
-            const data = editor.getData();
-            if (event.key === 'Enter'){
-                console.log( {  editor, data } ); 
-            }
-
-            console.log(  event); 
-            
-                // console.log( { event, editor, data } );
-            
-            
-        } } 
-       
+      <div
         style={{
-          
-          marginLeft:'12%',
-          marginTop:'5%',
+          marginLeft: "12%",
+          marginTop: "5%",
           width: "70%"
-        }}/>
+        }}
+      >
+        <CKEditor style={{height:'40wv'}}
+          editor={ClassicEditor}
+          data={this.state.value}
+          onChange={(event, editor) => {
+            const data = editor.getData();
 
-         
+            editor.editing.view.document.on("keydown", (evt, data) => {
+              console.log(data.keyCode);
 
-//         <Editor style={{minHeight: "600px",}}
-//   value = {this.state.value}
-//   toolbarClassName="toolbarClassName"
-//   wrapperClassName="wrapperClassName"
-//   editorClassName="editorClassName"
-//   onEditorStateChange={this.onEditorStateChange.bind(this)}
-//   mention={{
-//     separator: ' ',
-//     trigger: ' ',
-//     suggestions: [
-//       { text: 'APPLE onEditorStateChange={this.onEditorStateChange.bind(this)} ', value: 'APPLE onEditorStateChange={this.onEditorStateChange.bind(this)}'},
-//       { text: 'BANANA', value: 'banana', url: 'banana' },
-//       { text: 'CHERRY', value: 'cherry', url: 'cherry' },
-//       { text: 'DURIAN', value: 'durian', url: 'durian' },
-      
-//     ],
-//   }}
-//   onTab={(event) => {console.log("event",event)}}
-// />
-      
+              if (data.keyCode === 9) {
+                this.handleApiCall(editor.getData());
+
+                data.preventDefault();
+                evt.stop();
+              }
+              else{
+                this.setState({ token: false });
+              }
+            });
+          }}
+          
+        />
+
+        { this.state.token && 
+
+<List component="nav" style ={{marginLeft:"30%",marginRight:"20%",marginTop:"-20px"}}>
+{this.state.sentences.map((text, index) => (
+<ListItem button onClick={() => this.handleSelected(text.source,index)} key ={index}>
+  <ListItemText primary={text.source}/>
+</ListItem>
+))}
+
+
+
+  
+
+</List>
+
+
+        }
+        
+      </div>
     );
   }
 }
