143e56cd06ad811d367f8a7015b91abb64049dd1
meghaMS <meghana.suresh@tarento.com>
2020-04-27 22:51:19 +0530

    Added superscript

--- src/ui/containers/web/Interactive-Editor/EditorPaper.jsx
+++ src/ui/containers/web/Interactive-Editor/EditorPaper.jsx
@@ -12,22 +12,41 @@ const styles = {
 
 class EditorPaper extends React.Component {
 
+    fetchSuperScript(supArr) {
+        if(supArr && Array.isArray(supArr) && supArr.length > 0) {
+            return supArr.join()
+        } else {
+            return ''
+        }
+    }
+
     fetchSentence(sentence) {
         let align = sentence.align === 'CENTER' ? 'center' : (sentence.align === 'RIGHT' ? 'right' : 'left')
 
-        if ( sentence.is_ner && !sentence.is_new_line) {
-            return (<div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()} 
-                        style={{ backgroundColor: (this.props.selectedSentence === sentence._id) ? "yellow" : '', float: align, textAlign: align,display: 'inline-block',
-                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : '' }}>{sentence.text}</div>)
-        } else if (sentence.is_ner && sentence.is_new_line) {
-            return (<div><div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()} 
-                        style={{ backgroundColor: (this.props.selectedSentence === sentence._id) ? "yellow" : '', float: align, textAlign: align,
-                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : '' }}>{sentence.text}</div> <div style={{width: '100%'}}><br/>&nbsp;<br/></div></div>)
+        if (!sentence.is_footer && sentence.text) {
+            if (sentence.is_ner && !sentence.is_new_line) {
+                return (<div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()}
+                    style={{
+                        backgroundColor: (this.props.selectedSentence === sentence._id) ? "yellow" : '', float: align, textAlign: align, display: 'inline-block',
+                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : ''
+                    }}>{sentence.text}<sup>{this.fetchSuperScript(sentence.sup_array)}</sup></div>)
+            } else if (sentence.is_ner) {
+                return (<div><div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()}
+                    style={{
+                        backgroundColor: (this.props.selectedSentence === sentence._id) ? "yellow" : '', float: align, textAlign: align,
+                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : ''
+                    }}>{sentence.text}<sup>{this.fetchSuperScript(sentence.sup_array)}</sup></div> <div style={{ width: '100%' }}><br />&nbsp;<br /></div></div>)
+            } else {
+                return (<div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()}
+                    style={{
+                        backgroundColor: this.props.selectedSentence === sentence._id ? "yellow" : '', textAlign: align, right: 0,
+                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : ''
+                    }}>{sentence.text}<sup>{this.fetchSuperScript(sentence.sup_array)}</sup><br /><br /></div>)
+            }
         } else {
-            return (<div key={sentence._id} onClick={() => this.hoverOn(sentence._id)} onMouseEnter={() => this.hoverOn(sentence._id)} onMouseLeave={() => this.hoverOff()} 
-                        style={{ backgroundColor: this.props.selectedSentence === sentence._id ? "yellow" : '', textAlign: align, right: 0,
-                        fontWeight: sentence.is_bold ? 'bold' : 'normal', textDecorationLine: sentence.underline ? 'underline' : '' }}>{sentence.text}<br/><br/></div>)
+            return <div></div>
         }
+
     }
 
     hoverOn(e) {
@@ -40,12 +59,12 @@ class EditorPaper extends React.Component {
     }
 
     render() {
-        const { section, sentences, selectedSentence } = this.props;
+        const { section, sentences } = this.props;
         return (
 
             <div>
                 <div style={{ padding: "10px 24px 24px 24px" }}>
-                    <div variant="h6I" style={{ paddingBottom: "12px" }}>
+                    <div variant="h6" style={{ paddingBottom: "12px" }}>
                         {section}
                     </div>
 
