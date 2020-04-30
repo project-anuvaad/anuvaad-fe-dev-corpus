00a71ee9e0a95fed22cfdda80e1a068938b58588
sajish <sajish.sasidharan@tarento.com>
2020-04-30 19:01:07 +0530

    added save api

---
+++ src/flux/actions/apis/interactivesavesentence.js
@@ -0,0 +1,50 @@
+import API from "./api";
+import C from "../constants";
+
+
+export default class RunExperiment extends API {
+  constructor(sentences, timeout = 2000) {
+    console.log();
+    super("POST", timeout, false);
+    this.type = C.UPDATEINTERACTIVESENTENCE;
+    this.sentences = sentences;
+    
+    
+    
+  }
+
+  toString() {
+    return `${super.toString()} , type: ${this.type}`;
+  }
+
+  processResponse(res) {
+    super.processResponse(res);
+    if (res) {
+      this.sentences = res;
+    }
+  }
+
+  apiEndPoint() {
+    return `${super.apiEndPointAuto()}/update-pdf-sentences`;
+  }
+
+  getBody() {
+    return {
+        sentences:this.sentences
+    };
+  }
+
+  getHeaders() {
+    this.headers = {
+      headers: {
+        Authorization: `Bearer ${  decodeURI(localStorage.getItem("token"))}`,
+        "Content-Type": "application/json"
+      }
+    };
+    return this.headers;
+  }
+
+  getPayload() {
+    return this.sentences;
+  }
+}
