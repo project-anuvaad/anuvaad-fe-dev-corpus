0ff0838762b6f33bf899f2ea59ddf4a1d46eb921
aroop <aroop@netopss-MBP-6.Dlink>
2020-04-30 16:05:02 +0530

    scroll click added

--- src/ui/containers/web/Interactive-Editor/EditorPaper.jsx
+++ src/ui/containers/web/Interactive-Editor/EditorPaper.jsx
@@ -50,7 +50,7 @@ class EditorPaper extends React.Component {
             }
             tableRow.push(<tr>{col}</tr>)
         }
-        return <table style={{ marginBottom: '20px', border: '1px solid black', borderCollapse: 'collapse', width: '100%' }}>{tableRow}</table>
+        return <table ref={id + '_' + this.props.paperType} style={{ marginBottom: '20px', border: '1px solid black', borderCollapse: 'collapse', width: '100%' }}>{tableRow}</table>
     }
 
     fetchTokenizedSentence(sentence, isSpaceRequired) {
