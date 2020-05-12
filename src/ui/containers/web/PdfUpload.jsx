80584003635f941f30787488560b87d60b51fd64
sajish <sajish.sasidharan@tarento.com>
2020-05-12 12:11:00 +0530

    fixed interactive translate bug

--- src/ui/containers/web/PdfUpload.jsx
+++ src/ui/containers/web/PdfUpload.jsx
@@ -228,7 +228,7 @@ class PdfUpload extends Component {
           <Grid container spacing={24} style={{ marginLeft: "3%" }}>
             <Grid item xs={8} sm={8} lg={8} xl={8} style={{ paddingLeft: "2%" }}>
               <Typography gutterBottom variant="title" component="h2" style={{ width: "100%", paddingTop: "15px" }}>
-                Select Source Language:{" "}
+                Select Source Language *
               </Typography>
             </Grid>
             <Grid item xs={3} sm={3} lg={3} xl={3}>
@@ -257,7 +257,7 @@ class PdfUpload extends Component {
                 gutterBottom="true"
                 style={{ width: "100%", paddingTop: "15px", paddingBottom: "30px" }}
               >
-                Select Target Language:{" "}
+                Select Target Language *
               </Typography>
               <br />
             </Grid>
