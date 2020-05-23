2e25a4982a1ddeb564d47bfa2ee2607e81ad2564
aroop <aroop@netopss-MBP-6.Dlink>
2020-05-23 13:47:21 +0530

    account activation completed

--- src/ui/containers/web/Layout.jsx
+++ src/ui/containers/web/Layout.jsx
@@ -64,10 +64,10 @@ class App extends React.Component {
           {!dontShowLoader &&
             this.renderSpinner()
           }
-          {!dontShowHeader && 
-          <Header dontShowHeader={dontShowHeader} forDemo={forDemo || showLogo} classes={classes} theme={theme} title={title} drawer={drawer} tocken={this.state.tocken} handleTockenChange={this.handleTockenChange.bind(this)} />
+          {!dontShowHeader &&
+            <Header dontShowHeader={dontShowHeader} forDemo={forDemo || showLogo} classes={classes} theme={theme} title={title} drawer={drawer} tocken={this.state.tocken} handleTockenChange={this.handleTockenChange.bind(this)} />
           }
-         <div className={dontShowHeader ? '' :  (forDemo ? classes.containerDemo : classes.container)} onClick={this.handlDrawerTocken.bind(this)}>
+          <div style={dontShowHeader ? { width: '100%' } : {}} className={dontShowHeader ? '' : (forDemo ? classes.containerDemo : classes.container)} onClick={this.handlDrawerTocken.bind(this)}>
             {/* {forDemo &&
               <div style={{
                 position: 'absolute',
