727e4194a40c92f22511ee0bad78b1582e019290
meghaMS <meghana.suresh@tarento.com>
2020-08-20 20:05:24 +0530

    Table changes

--- src/ui/containers/web/Interactive-Editor/EditorTable.jsx
+++ src/ui/containers/web/Interactive-Editor/EditorTable.jsx
@@ -28,18 +28,22 @@ class EditorTable extends React.Component {
         let index = 0;
 
         let col = [];
+        console.log(sentences.children.length)
+        console.log(sentences.children[sentences.children.length-1])
 
+        console.log(sentences.children[sentences.children.length])
         if (sentences && sentences.children && Array.isArray(sentences.children) && sentences.children.length > 0) {
             sentences.children.map((tableData, i) => {
                 if (tableData.text && Array.isArray(tableData.text) && tableData.text.length > 0) {
-                    console.log(tableData.text_width)
-                    debugger
+                    console.log(tableData)
                     col.push(
                         <td key={i}
                             style={{
                                 // width: tableData.text_width + "px",
                                 border: "1px solid black", borderCollapse: "collapse"
                             }}>{this.fetchBlockData(tableData.text)}</td>)
+
+
                     // tableData.text.map((data, index) => {
 
                     // col.push(
@@ -79,7 +83,18 @@ class EditorTable extends React.Component {
         const { table } = this.props
         return (
             <div>
-                <table style={{ border: "1px solid black", borderCollapse: "collapse", position: "absolute", top: table.text_top + "px", left: table.text_left + "px", width: table.text_width + "px" }}>
+                <table
+                    style={{
+                        border: "1px solid black",
+                        borderCollapse: "collapse",
+                        position: "absolute",
+                        top: table.text_top + "px",
+                        left: table.text_left + "px",
+                        width: table.text_width + "px",
+                        height: table.text_height + "px"
+                    }}>
+                        {/* <td style={{left: "100px", top: "50px", border: "1px solid black"}}>Test1</td> */}
+                        {/* <td>Test2</td> */}
                     <tbody>{this.fetchTableContent(table)}</tbody>
                 </table>
             </div>
