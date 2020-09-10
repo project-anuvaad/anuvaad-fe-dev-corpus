6d041f933b66fc1da080ed34fd4797228e7f4b8c
meghaMS <meghana.suresh@tarento.com>
2020-09-10 15:36:37 +0530

    Hover to sentence

--- src/ui/containers/web/Interactive-Editor/DocumentBlock.jsx
+++ src/ui/containers/web/Interactive-Editor/DocumentBlock.jsx
@@ -15,12 +15,12 @@ class Preview extends React.Component {
         }
     }
 
-    handleCheck = event =>{
+    handleCheck = event => {
         this.props.handleCheck(this.props.sentence.block_id + "_" + this.props.page_no, event, false)
     }
 
     handleChangeEvent = event => {
-       
+
         this.props.handleSourceChange(this.props.sentence.block_id + "_" + this.props.page_no, event, this.props.sentence)
     }
 
@@ -33,26 +33,24 @@ class Preview extends React.Component {
             fontSize: sentence.font_size + "px",
             color: sentence.font_color,
             width: sentence.text_width + "px",
-            fontFamily : sentence.font_family,
             fontWeight: sentence.font_family && sentence.font_family.includes("Bold") && 'bold',
             textAlign: "justify",
             zIndex: 1,
-            cursor: !this.state.isEditable &&'pointer',
+            cursor: !this.state.isEditable && 'pointer',
             lineHeight: sentence.children && parseInt(sentence.text_height / sentence.children.length) + 'px',
             // backgroundColor: this.props.hoveredSentence === this.props.sentence.block_id + "_" + this.props.page_no ? "yellow" : ""
             backgroundColor: this.props.hoveredSentence === this.props.sentence.block_id + "_" + this.props.page_no && !this.props.selectedBlock ? "yellow" : ""
         }
-      
+
         return (
             <div id={sentence.block_id + "_" + this.props.page_no} style={styles} key={sentence.block_id}
-                onMouseEnter={() => this.handleMouseHover(sentence.block_id + "_" + this.props.page_no)}
-                onMouseLeave={() => this.props.handleOnMouseEnter()}
+
                 onDoubleClick={event => this.props.handleDoubleClick(sentence.block_id + "_" + this.props.page_no, event, sentence)}
                 // contentEditable = {this.props.createBlockId === sentence.block_id + "_" + this.props.page_no ? true : false}
                 onClick={() => {
                     if (sentence.block_id + "_" + this.props.page_no !== this.props.selectedBlock) {
                         this.props.handleBlockClick(false, sentence.block_id + "_" + this.props.page_no)
-                    } else if(sentence.block_id + "_" + this.props.page_no !== this.props.createBlockId) {
+                    } else if (sentence.block_id + "_" + this.props.page_no !== this.props.createBlockId) {
 
                         console.log(sentence.block_id + "_" + this.props.page_no)
                         this.props.handleEditor(sentence.block_id + "_" + this.props.page_no)
@@ -61,14 +59,14 @@ class Preview extends React.Component {
             >
                 {(this.props.selectedBlock === sentence.block_id + "_" + this.props.page_no || this.props.createBlockId === sentence.block_id + "_" + this.props.page_no) ? (
                     <ContentEditable
-                    autoComplete = "off"
-                    html={this.props.selectedSourceText}
+                        autoComplete="off"
+                        html={this.props.selectedSourceText}
                         disabled={false}
                         onBlur={this.handleCheck}
                         onChange={this.handleChangeEvent}
                         style={{
                             border: "1px solid #1C9AB7",
-                            
+
                             cursor: 'auto',
                             backgroundColor: "#F4FDFF",
                             outline: "none !important"
@@ -76,8 +74,15 @@ class Preview extends React.Component {
                         }}
                     />
                 ) : (
-                    sentence.hasOwnProperty('tokenised_text') ? sentence.tokenised_text.map(text=>text) : sentence.text
-                    
+                        sentence.hasOwnProperty('tokenised_text') ? sentence.tokenised_text.map((text, tokenIndex) => {
+                            return <span style={{ backgroundColor: this.props.hoveredSentence === tokenIndex + "_" + sentence.block_id + "_" + this.props.page_no }}
+                                onMouseLeave={() => this.props.handleOnMouseEnter()}
+                                onMouseEnter={() => this.handleMouseHover(tokenIndex + "_" + sentence.block_id + "_" + this.props.page_no)}>{text}</span>
+                        }) : <div
+                            onMouseLeave={() => this.props.handleOnMouseEnter()}
+                            onMouseEnter={() => this.handleMouseHover(sentence.block_id + "_" + this.props.page_no)}
+                            style={{ backgroundColor: this.props.hoveredSentence === sentence.block_id + "_" + this.props.page_no }}>{sentence.text}</div>
+
                     )}
             </div >
         );
