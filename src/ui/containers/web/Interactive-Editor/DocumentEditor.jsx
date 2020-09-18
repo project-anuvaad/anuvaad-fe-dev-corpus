8f4bbd74e7823c6bcec799100cdb36ae6d406474
meghaMS <meghana.suresh@tarento.com>
2020-09-18 14:27:35 +0530

    Changes for pagination

--- src/ui/containers/web/Interactive-Editor/DocumentEditor.jsx
+++ src/ui/containers/web/Interactive-Editor/DocumentEditor.jsx
@@ -724,7 +724,7 @@ class PdfFileEditor extends React.Component {
         }
       });
     let paperWidth = this.state.sentences && this.state.sentences[0].page_width - leftPaddingValue - 78 + "px";
-
+     
     return (
       <div>
         <Grid container spacing={8} style={{ padding: "0 24px 12px 24px" }}>
@@ -782,9 +782,9 @@ class PdfFileEditor extends React.Component {
             <Paper
               elevation={2}
               style={{
-                maxHeight: this.state.collapseToken ? window.innerHeight - 120 : window.innerHeight - 200,
+                // maxHeight: this.state.collapseToken ? window.innerHeight - 120 : window.innerHeight - 200,
                 paddingBottom: "12px",
-                overflow: "scroll"
+                // overflow: "scroll"
               }}
             >
               <Toolbar style={{ color: darkBlack, background: blueGrey50 }}>
@@ -792,7 +792,11 @@ class PdfFileEditor extends React.Component {
                   Extracted Document
                 </Typography>
               </Toolbar>
-              <div>
+              <div id="scrollableDiv"  style={{
+                maxHeight: window.innerHeight - 240,
+                // paddingBottom: "12px",
+                overflow: "scroll"
+              }}>
                 <InfiniteScroll
                   next={this.fetchData.bind(this)}
                   hasMore={this.state.hasMoreItems}
@@ -821,7 +825,7 @@ class PdfFileEditor extends React.Component {
                   {this.state.sentences &&
                     this.state.sentences.map((sentence, index) => {
                       return (
-                        <div>
+                        // <div>
                           <SourceView
                             isPreview={true}
                             key={sentence.page_no + "_" + index}
@@ -856,7 +860,7 @@ class PdfFileEditor extends React.Component {
                             tokenized={this.state.tokenized}
                             pageCount={this.state.pageCount}
                           />
-                        </div>
+                        // </div>
                       );
                     })}
                 </InfiniteScroll>
@@ -865,9 +869,9 @@ class PdfFileEditor extends React.Component {
           </Grid>
           <Grid item xs={12} sm={6} lg={6} xl={6} style={{ padding: "8px" }}>
             <Paper  style={{
-                maxHeight: this.state.collapseToken ? window.innerHeight - 120 : window.innerHeight - 200,
+                // maxHeight: this.state.collapseToken ? window.innerHeight - 120 : window.innerHeight - 200,
                 paddingBottom: "12px",
-                overflow: "scroll"
+                // overflow: "scroll"
               }}>
               <DocPreview
                 parent="document-editor"
