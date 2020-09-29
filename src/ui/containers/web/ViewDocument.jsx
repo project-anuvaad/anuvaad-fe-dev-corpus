62784a1ef56d765d881bce15fa0a665bc015df65
meghaMS <meghana.suresh@tarento.com>
2020-09-29 13:34:15 +0530

    Changes date format

--- src/ui/containers/web/ViewDocument.jsx
+++ src/ui/containers/web/ViewDocument.jsx
@@ -63,10 +63,9 @@ class ViewDocument extends React.Component {
         let date = value.startTime.toString()
         let timestamp = date.substring(0, 13)
         var d = new Date(parseInt(timestamp))
-        // let dateStr = d.toISOString()
-        // var myDate = new Date(t.createdAt);
-        // t.createdAt = (myDate.toLocaleString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }))
-
+        let dateStr = d.toISOString()
+        var myDate = new Date(dateStr);
+        let createdAt = (myDate.toLocaleString('en-US', { day: '2-digit', month: '2-digit', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }))
 
         let sourceLangCode, targetLangCode, sourceLang, targetLang
         if (value && value.input && value.input.files && value.input.files.length > 0 && value.input.files[0].model && value.input.files[0].model.source_language_code && value.input.files[0].model.target_language_code) {
@@ -95,7 +94,7 @@ class ViewDocument extends React.Component {
         b["inputFile"] = value.taskDetails && value.taskDetails.length > 0 && value.taskDetails[0].output && value.taskDetails[0].output.length > 0 && value.taskDetails[0].output[0].outputFile;
         b["modelId"] = value && value.input && value.input.files && value.input.files.length > 0 && value.input.files[0].model && value.input.files[0].model.model_id
         b["locale"] = value && value.input && value.input.files && value.input.files.length > 0 && value.input.files[0].model && value.input.files[0].model.source_language_code
-        b["timestamp"] = d.toISOString()
+        b["timestamp"] = createdAt
         b["source"] = sourceLang
         b["target"] = targetLang
 
