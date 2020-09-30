421335bc12d575bec40b885542f0df208f04972d
meghaMS <meghana.suresh@tarento.com>
2020-09-30 11:40:37 +0530

    Changes for table

--- src/ui/containers/web/ViewDocument.jsx
+++ src/ui/containers/web/ViewDocument.jsx
@@ -185,7 +185,7 @@ class ViewDocument extends React.Component {
             if (tableMeta.rowData) {
               return (
                 <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>
-                  {tableMeta.rowData[3]}
+                  <div style={tableMeta.rowData[1] === 'COMPLETED' ? {cursor: "pointer", width: "fit-content"} : {}}>{tableMeta.rowData[3]}</div>
                 </div>
               );
             }
@@ -238,7 +238,7 @@ class ViewDocument extends React.Component {
             if (tableMeta.rowData) {
               return (
                 <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>
-                  {tableMeta.rowData[8]}
+                  <div style={tableMeta.rowData[1] === 'COMPLETED' ? {cursor: "pointer", width: "fit-content"} : {}}>{tableMeta.rowData[8]}</div>
                 </div>
               );
             }
@@ -257,7 +257,7 @@ class ViewDocument extends React.Component {
             if (tableMeta.rowData) {
               return (
                 <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>
-                  {tableMeta.rowData[9]}
+                  <div style={tableMeta.rowData[1] === 'COMPLETED' ? {cursor: "pointer", width: "fit-content"} : {}}>{tableMeta.rowData[9]}</div>
                 </div>
               );
             }
@@ -281,7 +281,8 @@ class ViewDocument extends React.Component {
 
                 <div style={{ width: '120px' }}>
 
-                  {(tableMeta.rowData[1] !== 'COMPLETED' && tableMeta.rowData[1] !== 'FAILED') ? <ProgressBar token={true} val={1000} eta={2000 * 1000} handleRefresh={this.handleRefresh.bind(this)}></ProgressBar> : <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>{tableMeta.rowData[1]}</div>}
+                  {(tableMeta.rowData[1] !== 'COMPLETED' && tableMeta.rowData[1] !== 'FAILED') ? <ProgressBar token={true} val={1000} eta={2000 * 1000} handleRefresh={this.handleRefresh.bind(this)}></ProgressBar> : <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>
+                  <div style={tableMeta.rowData[1] === 'COMPLETED' ? {cursor: "pointer", width: "fit-content"} : {}}>{tableMeta.rowData[1]}</div></div>}
 
                 </div>
               );
@@ -301,7 +302,7 @@ class ViewDocument extends React.Component {
             if (tableMeta.rowData) {
               return (
                 <div onClick={() => tableMeta.rowData[1] === 'COMPLETED' && this.handleClick(tableMeta.rowData)}>
-                  {tableMeta.rowData[11]}
+                  <div style={tableMeta.rowData[1] === 'COMPLETED' ? {cursor: "pointer", width: "fit-content"} : {}}>{tableMeta.rowData[11]}</div>
                 </div>
               );
             }
@@ -356,7 +357,7 @@ class ViewDocument extends React.Component {
       filterType: "checkbox",
       // onRowClick: rowData => (rowData[1] === "COMPLETED") && this.handleClick(rowData),
       download: false,
-      expandableRowsOnClick: true,
+      // expandableRowsOnClick: true,
       print: false,
       fixedHeader: true,
       filter: false,
