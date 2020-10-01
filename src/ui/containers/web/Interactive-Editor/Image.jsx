6accd03e7e7cc7df2b4534a9897046b0eaab060a
meghaMS <meghana.suresh@tarento.com>
2020-10-01 12:12:51 +0530

    Removed unused import

--- src/ui/containers/web/Interactive-Editor/Image.jsx
+++ src/ui/containers/web/Interactive-Editor/Image.jsx
@@ -9,7 +9,7 @@ class Image extends React.Component {
           const { imgObj} = this.props;
           return(
             <div style={{position: "absolute", left: imgObj.text_left + "px", top: imgObj.text_top + "px", width: imgObj.text_width + "px"}}>
-                <img width={imgObj.text_width + "px"} height={imgObj.text_height + "px"} src={`data:image/png;base64,${imgObj.base64}`}></img>
+                <img width={imgObj.text_width + "px"} height={imgObj.text_height + "px"} src={`data:image/png;base64,${imgObj.base64}`} alt=""></img>
             </div>
           )
       }
